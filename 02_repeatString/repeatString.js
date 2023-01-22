c8a7c82079103d0312662f4a2e7be110661513b4
yassin <yassinhussein241@gmail.com>
2023-01-22 18:28:29 -0500

    Add Repeat Function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+function repeatString(strings, num) {
+    let addString = "";
+    for (let i = 0; i < num; i++){
+        addString += strings;
+    }
+    return addString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
