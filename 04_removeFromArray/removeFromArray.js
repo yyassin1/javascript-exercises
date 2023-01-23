f9424326b6a39dc64b69d7857a2a5b2601fd8005
yassin <yassinhussein241@gmail.com>
2023-01-23 12:09:29 -0500

    Add RemoveFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+let array = [];
+function removeFromArray(array, ...args) {
+    arrayLength = array.length;
+    let argsLength = args.length;
+    for (let i = 0; i < arrayLength; i++){
+        for (let j = 0; j < argsLength + 1; j++){
+            if (array[i] === args[j]){
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
