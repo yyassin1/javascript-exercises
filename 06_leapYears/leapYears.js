e005ae516b1557fd375a304dfd6805940250ef4c
yassin <yassinhussein241@gmail.com>
2023-01-23 12:48:12 -0500

    Add leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+function leapYears(year) {
+    if (year % 4 == 0 && year % 400 == 0){
+        return true;
+    }
+    if (year % 4 == 0 && year % 100 != 0){
+        return true;s
+    }
+    else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
