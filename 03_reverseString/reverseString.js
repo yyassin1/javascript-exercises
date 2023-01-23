bf7af31db4e36226e61907f84bbe1ffa13f7917c
yassin <yassinhussein241@gmail.com>
2023-01-22 19:08:23 -0500

    Add Reverse Function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
-};
+function reverseString(string) {
+    stringLength = string.length;
+    let addString = [];
+    let j = stringLength;
+    for (let i = 0; i < stringLength; i++){
+        addString[j - 1] = string[i];
+        j--;
+    }
+    return addString.join('');
+}
 
 // Do not edit below this line
 module.exports = reverseString;
