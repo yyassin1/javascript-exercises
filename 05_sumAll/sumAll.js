425683427e6d27a5f725d3d4f42b33f1beac8d08
yassin <yassinhussein241@gmail.com>
2023-01-23 12:33:43 -0500

    Add SumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+function sumAll(firstNum, lastNum) {
+    let sum = 0;
+    if (firstNum <= 0 || lastNum <= 0 || typeof firstNum != 'number' || typeof lastNum != 'number' )
+    {
+        return 'ERROR';
+    }
+    if (firstNum < lastNum){
+        for (let i = firstNum; i <= lastNum; i++){
+            sum = sum + i;
+        }
+    }
+    else {
+        for (let i = firstNum; i >= lastNum; i--){
+            sum = sum + i;
+        }
+    }
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
