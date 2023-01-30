204df85a5cb4b8d582e272156a5d8bd2c348349f
yassin <yassinhussein241@gmail.com>
2023-01-30 12:02:47 -0500

    Edit Calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(sums) {
+  let total = 0;
+	for (let i = 0; i < sums.length; i++){
+    total = total + sums[i];
+  } 
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(mult) {
+  let total = 1;
+	for (let i = 0; i < mult.length; i++){
+    total = total * mult[i];
+  } 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+	for (let fac = a; fac >= 1; fac--){
+    result *= fac;
+  }
+  return result;
 };
 
 // Do not edit below this line
